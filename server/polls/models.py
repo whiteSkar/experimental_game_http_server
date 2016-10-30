e1f825ae2c1c92273803c22d51b94b1577205322
whiteSkar <sypower07@hotmail.com>
2016-10-29 23:21:22 -0700

    part 2 of django tutorial

--- server/polls/models.py
+++ server/polls/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=256)
+    date_published = models.DateTimeField()
+
+    def __str__(self):
+        return self.question_text
+
+    def is_published_recently(self):
+        return self.date_published >= timezone.now() - datetime.timedelta(days=1) 
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=128)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text

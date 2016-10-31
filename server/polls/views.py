1bc7682101b851478fec6a6595182a04708345bd
whiteSkar <sypower07@hotmail.com>
2016-10-30 22:18:30 -0700

    tutorial 5

--- server/polls/views.py
+++ server/polls/views.py
@@ -1,32 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
 
-def index(request):
-    recent_questions = Question.objects.order_by('-date_published')[:5]
-    context = {
-        'recent_questions': recent_questions,
-    }
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'recent_questions'
 
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.order_by('-date_published')[:5]
 
-def detail(request, question_id):
+
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
+
+
+def vote(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question %s does not exist" % question_id)
 
-    context = {
-        'question': question,
-    }
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+   
+    selected_choice.votes += 1
+    selected_choice.save()
+    
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-    return render(request, 'polls/detail.html', context)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
